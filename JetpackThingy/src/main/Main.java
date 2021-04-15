fd963326351194f58767cc4ce4853b01c64f671b
soeren <soeren@DESKTOP-NCU3OCN>
2021-04-15 09:55:17 +0200

    Basic-Engine Setup

---
+++ JetpackThingy/src/main/Main.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.Color;
+
+import engine.game.AbstractGame;
+import engine.game.GameContainer;
+import engine.gameobjects.GameObject;
+import engine.gameobjects.gamebehaviour.type.GameBehaviour;
+import engine.math.Vector2;
+import engine.scenes.Scene;
+import engine.scenes.SceneManager;
+
+public class Main extends AbstractGame {
+	
+	Scene inGame;
+	
+	public static void main(String[] args) {
+		GameContainer gc = new GameContainer(new Main());	
+		gc.setSize(new Vector2(1000, 500));
+		gc.start();
+	}
+	
+	public Main() {
+		inGame = new Scene("inGame") {
+
+			@Override
+			public void instanceGameObjects() {
+				GameObject würfelTest = new GameObject(new Vector2(0), true);
+				würfelTest.addComponent(new GameBehaviour() {
+					public void render() {
+						this.d.setColor(Color.WHITE);
+						this.d.fillRect(new Vector2(100));
+					}
+				});
+				this.addGameObject(würfelTest);
+				
+			}
+			
+		};
+	}
+
+	@Override
+	public void start() {
+		SceneManager.loadScene(inGame);
+	}
+
+}
