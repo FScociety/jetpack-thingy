08b5d00aa549230ed529909fe26a6449ebb4a21a
sorenschmidt-clausen <sorenschmidt-clausen@qq.fritz.box>
2021-05-05 11:33:00 +0200

    anpassung an Engine Update

--- JetpackThingy/src/main/Main.java
+++ JetpackThingy/src/main/Main.java
@@ -1,12 +1,14 @@
 package main;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
 
 import engine.game.AbstractGame;
 import engine.game.GameContainer;
 import engine.gameobjects.GameObject;
-import engine.gameobjects.gamebehaviour.builtin.animation.SpriteAnimation;
-import engine.gameobjects.gamebehaviour.builtin.animation.SpriteSheet;
+import engine.gameobjects.gamebehaviour.builtin.animation.SpriteAnimator;
+import engine.gameobjects.gamebehaviour.builtin.animation.SpriteAnimationSheet;
+import engine.gameobjects.gamebehaviour.builtin.camera.CameraController;
 import engine.gameobjects.gamebehaviour.type.GameBehaviour;
 import engine.math.Vector2;
 import engine.scenes.Scene;
@@ -27,12 +29,26 @@ public class Main extends AbstractGame {
 
 			@Override
 			public void instanceGameObjects() {
-				GameObject würfelTest = new GameObject(new Vector2(0), true);
-				SpriteSheet ss = new SpriteSheet("/animationTest.png", 15, 1);
-				würfelTest.addComponent(new SpriteAnimation(ss));
-				this.addGameObject(würfelTest);
-				würfelTest.setScale(new Vector2(10));
+				GameObject wuerfelTest = new GameObject(new Vector2(0), true);
+				int[] arrangement = {15, 15};
+				SpriteAnimationSheet ss = new SpriteAnimationSheet("/animationTest.png", arrangement, new Vector2(10, 10));       
+				SpriteAnimator spriteAnim = new SpriteAnimator(ss);
+				wuerfelTest.addComponent(spriteAnim);
+				wuerfelTest.addComponent(new GameBehaviour() {
+					SpriteAnimator sa = spriteAnim;
+					
+					public void update() {
+						if (GameContainer.input.isKey(KeyEvent.VK_SPACE)) {
+							sa.playAfterFinish(1);
+						}
+					}
+				});
+				spriteAnim.playInstant(0);
+				this.addGameObject(wuerfelTest);
+				wuerfelTest.setScale(new Vector2(10));
 				
+				
+				this.defaultCamera.gameObject.addComponent(new CameraController(true, true, false));
 			}
 			
 		};
